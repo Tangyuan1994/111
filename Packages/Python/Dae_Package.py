4cbe95cb772dfcc62962be14928c20f58fcdad9e
Florian-Bertot <florian.bertot@gmail.com>
2017-06-30 11:15:42 +0200

    Add Python Connexion

--- Packages/Python/Dae_Package.py
+++ Packages/Python/Dae_Package.py
@@ -2,16 +2,33 @@ import requests
 
 distantAdress = "http://localhost:3000/"
 
+
 def getData(id):
     return requests.get(distantAdress+"datasets/"+id)
 
 print(getData("all").text)
 print(getData("5330e2f3a0dd8ee433e3e702df012c19").text)
 
+
 def postRate(id, rate):
     return requests.post(distantAdress+"document/"+id+"/addRate/"+str(rate))
 
-print("\n","\n Ajout d'une note :",postRate("5330e2f3a0dd8ee433e3e702df012c19", 4).text)
+print("\n Ajout d'une note :", postRate("5330e2f3a0dd8ee433e3e702df012c19", "4.5").text)
+
+
+def connexion(username, password):
+    return requests.get(distantAdress+'connexion/'+username+'/'+password)
+
+token = connexion('capfloy77@gmail.com', '123456').text.strip("\"")
+
+
+def getDataWithToken(tok):
+    return requests.get(distantAdress + 'getData/'+str(tok))
+
+print("\n", getDataWithToken(token).text)
+print("\n", getDataWithToken("qduhjndsqhbnqdsgahn8752sqdh").text)
+
+
 
 
 
