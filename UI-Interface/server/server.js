7cf7ff237264f2a644c4dc5451ac2dbce766bd8d
cpnu <constance.pineau@telecomnancy.eu>
2017-07-21 15:17:31 +0200

    Merge branch 'master' of /Users/Constance/Documents/Dae-Ng with conflicts.

--- UI-Interface/server/server.js
+++ UI-Interface/server/server.js
@@ -47,9 +47,13 @@ router.post('/postData', function (req, res, next) {
         .then(function(response){
             console.log(response)
             o.postESData(req.body.data)
-                .then(function (response) {
+                .then(function (response) { //return ArrayOf({Position in Data,ID of object})
                     console.log(response)
-                    o.postCouchData(response.id, req.body.data)
+                    tab =[]
+                    for (var i=0; i<response.length; i++){
+                        tab.push({id:response[i].id, data:req.body.data[response[i].pos]})
+                    }
+                    o.postCouchData(tab)
                         .then(function(response){
                             console.log(response)
                             res.json(response)
@@ -78,13 +82,24 @@ router.post('/postFiles', function (req, res, next) {
     o.checkToken(req.body.token)
         .then(function(response){
             console.log(response)
-            o.postESData(req.body.data)
-                .then(function (response) {
-                    console.log(response)
-                    o.postAttachment(req.body.data, response.id)
-                        .then(function(response){
-                            console.log(response)
-                            res.json(response)
+            o.uploadFiles(req)
+                .then(function(response){
+                    console.log(response) //Array[{Name, NewPath}]
+                    o.postESData(req.body.data) // ArrayOf(JSON)
+                        .then(function (response2) {
+                            console.log(response2) //ArrayOf({Position in Data,ID of object})
+                            tab=[]
+                            for (var i=0; i<response2.length; i++){
+                                tab.push({id:response2[i].id, path: response[i].newpath, data:req.body.data[response2[i].pos]})
+                            }
+                            o.postAttachment(tab) //datas = Array[{ID,Paths,Data}]
+                                .then(function(response){
+                                    console.log(response)
+                                    res.json(response)
+                                })
+                                .catch(function(error){
+                                    console.log(error)
+                                })
                         })
                         .catch(function(error){
                             console.log(error)
@@ -100,6 +115,27 @@ router.post('/postFiles', function (req, res, next) {
 
 });
 
+/**
+ * Use ES to get the data without attachment
+ * @param ES query
+ * @returns send response of Elasticsearch data without attachment
+ */
+router.get('/getData', function (req, res,next) {
+    o.checkToken(req.body.token)
+        .then(function(response) {
+            console.log(response)
+            o.getESData(req.body.data.query)
+                .then(function(response) {
+                    res.json(response)
+                })
+                .catch(function(error){
+                    console.log(error)
+                })
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+});
 
 /**
  * Get the file with the ID id
@@ -111,10 +147,24 @@ router.get('/getFiles', function(req, res, next){
     o.checkToken(req.body.token)
         .then(function(response){
             console.log(response)
-            o.getESId(req.body.data.query)
-                .then(function(response){
-                    console.log(response)
-                    o.getAttachment(response)
+            if ('_source' in req.body.data.query.query ) {
+                if (req.body.data.query.query._source.indexOf('doc._id') < 0){
+                    req.body.data.query.query._source.push('doc._id')
+                }
+                if (req.body.data.query.query._source.indexOf('doc.name') < 0){
+                    req.body.data.query.query._source.push('doc.name')
+                }
+            }
+            else {
+                req.body.data.query.query._source=[doc._id, doc.name]
+            }
+            o.getESData(query)
+                .then(function(response){ //response = array of data => id, name
+                    var data = []
+                    for (var i=0; i<response.length; i++){
+                        data.push({id:response[i]._id, name:response[i].name})
+                    }
+                    o.getAttachment(data)
                         .then(function(response){
                             console.log(response)
                             res.json(response)
