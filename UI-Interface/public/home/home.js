ed6a2088382825a1edfe40458102722585fed0d7
Tangyuan1994 <2656995835@qq.com>
2017-07-31 10:32:22 +0200

    Update home.js

--- UI-Interface/public/home/home.js
+++ UI-Interface/public/home/home.js
@@ -61,3 +61,22 @@ angular.module('daeNG')
 
       };
 
+
+
+
+    });
+
+
+angular.module("daeNG")
+    .directive('wjValidationError', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctl) {
+            scope.$watch(attrs['wjValidationError'], function (errorMsg) {
+                elm[0].setCustomValidity(errorMsg);
+                ctl.$setValidity('wjValidationError', errorMsg ? false : true);
+            });
+        }
+    };
+});
+
